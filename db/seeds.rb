9b3782648da22e8c645b15e825e2709173b786ea
Abdul Muneem <r.abdulmuneem@gmail.com>
2022-02-10 23:09:53 +0500

    Added project workflows

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Project.destroy_all
+
+user = User.create(email: 'a@abc.com', password: 'aaaaaa')
+3.times do |p|
+  user.projects.create(title: "Project ##{p}")
+end

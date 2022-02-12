a0517cf4c47b21df9644aef37fc792d4b2a559ee
Abdul Muneem <r.abdulmuneem@gmail.com>
2022-02-13 02:06:18 +0500

    Devs can now mark assigned change requests as resolved

--- app/controllers/developer/change_requests_controller.rb
+++ app/controllers/developer/change_requests_controller.rb
@@ -1,11 +1,27 @@
 class Developer::ChangeRequestsController < ApplicationController
+  before_action :set_change_request, only: %i[show assign update]
+
   def show
   end
 
   def assign
-    change = ChangeRequest.find(params[:id])
     dev = Developer.find(current_user.id)
-    change.developers << dev unless change.developers.include? dev
-    redirect_to developer_project_path(change.project)
+    @change.developers << dev unless @change.developers.include? dev
+    @change.update(status: 'started')
+    redirect_to developer_project_path(@change.project)
   end
+
+  def update
+    if (@change.type == 'Bug')
+      @change.update(status: 'resolved')
+    else
+      @change.update(status: 'completed')
+    end
+    redirect_to developer_project_path(@change.project)
+  end
+
+  private
+    def set_change_request
+      @change = ChangeRequest.find(params[:id])
+    end
 end
